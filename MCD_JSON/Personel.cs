9270c0cec0dad6094474930016a4202eac0fc67d
emre gemici <gmcc.emree@gmail.com>
2021-12-22 21:04:07 +0300

    Add project files.

---
+++ MCD_JSON/Personel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_JSON
+{
+    public class Personel
+    {
+        public Guid ID { get; set; }
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public string Adres { get; set; }
+        public string EmailAdres { get; set; }
+        public string TelefonNo { get; set; }
+    }
+}
---
+++ MCD_JSON/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MCD_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Personel> Personellerim = new List<Personel>();
+            for (int i = 0; i < 1000; i++)
+            {
+                Personel P1 = new Personel();
+                P1.ID = Guid.NewGuid();
+                P1.Isim = FakeData.NameData.GetFirstName();
+                P1.Soyisim = FakeData.NameData.GetSurname();
+                P1.EmailAdres = $"{P1.Isim}.{P1.Soyisim}@{FakeData.NetworkData.GetDomain()}";
+                P1.TelefonNo = FakeData.PhoneNumberData.GetPhoneNumber();
+
+                Personellerim.Add(P1);
+
+            }
+
+            Console.WriteLine("Bilgileriniz JSON formatında kaydedilecektir.");
+
+            //http: //jsonviewer.stack.hu/
+            if (Directory.Exists(@"c:\\JsonIslemlerim\\"))
+            {
+
+            }
+            else
+            {
+                Directory.CreateDirectory(@"c:\\JsonIslemlerim\\");
+            }
+            string JsonPersonellerim = Newtonsoft.Json.JsonConvert.SerializeObject(Personellerim);
+            File.WriteAllText("c:\\JsonIslemlerim\\Personellerim.json", JsonPersonellerim);
+        }
+    }
+}
