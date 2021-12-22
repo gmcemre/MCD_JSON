b763d221accd523c65d53f4b16660659151f5016
emre gemici <gmcc.emree@gmail.com>
2021-12-22 21:17:16 +0300

    code changed

---
+++ MCD_JSONOkuma/Personel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_JSONOkuma
+{
+    public class Personel
+    {
+        public Guid ID { get; set; }
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public string Adres { get; set; }
+        public string EmailAdres { get; set; }
+        public string TelefonNo { get; set; }
+
+        public override string ToString()
+        {
+            return Isim + " " + Soyisim;
+        }
+    }
+}
---
+++ MCD_JSONOkuma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MCD_JSONOkuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string JsonOkumaData = File.ReadAllText("c:\\JsonIslemlerim\\Personellerim.json");
+            List<Personel> Data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Personel>>(JsonOkumaData);
+
+            for (int i = 0; i < 50; i++)
+            {
+                //Console.WriteLine(Data[i].ID);
+                Console.WriteLine(Data[i]);
+                //Console.WriteLine(Data[i].Soyisim);
+                //Console.WriteLine(Data[i].Adres);
+                //Console.WriteLine(Data[i].EmailAdres);
+                //Console.WriteLine(Data[i].TelefonNo);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
